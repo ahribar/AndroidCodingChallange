4808ee76997d70816828c1fe1ffc654a5989db8c
Andraž <andraz.hribar@gmail.com>
2019-11-03 21:38:22 +0100

    Add basic fragments and routes

---
+++ app/src/main/java/si/development/ahill/beeniusdemo/presentation/albums/AlbumsFragment.kt
@@ -0,0 +1,22 @@
+package si.development.ahill.beeniusdemo.presentation.albums
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import si.development.ahill.beeniusdemo.R
+
+class AlbumsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_albums, container, false)
+    }
+
+
+}
