d676e979ed1cc153f2028ca0aeaaf5d0e1808f23
Andraž <andraz.hribar@gmail.com>
2019-11-04 20:15:01 +0100

    Add rest models

---
+++ app/src/main/java/si/development/ahill/beeniusdemo/data/rest/models/AlbumRestModel.kt
@@ -0,0 +1,7 @@
+package si.development.ahill.beeniusdemo.data.rest.models
+
+data class AlbumRestModel(
+    val id: Long?,
+    val title: String?,
+    val userId: Long?
+)
---
+++ app/src/main/java/si/development/ahill/beeniusdemo/data/rest/models/PhotoRestModel.kt
@@ -0,0 +1,9 @@
+package si.development.ahill.beeniusdemo.data.rest.models
+
+data class PhotoRestModel(
+    val albumId: Long?,
+    val id: Long?,
+    val thumbnailUrl: String?,
+    val title: String?,
+    val url: String?
+)
