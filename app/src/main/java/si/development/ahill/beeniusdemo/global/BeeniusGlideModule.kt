1df9f23726766038cd940ea3d6a138615efb0517
Andraž <andraz.hribar@gmail.com>
2019-11-09 23:56:33 +0100

    Fixed some dependency/build issues

---
+++ app/src/main/java/si/development/ahill/beeniusdemo/global/BeeniusGlideModule.kt
@@ -0,0 +1,11 @@
+package si.development.ahill.beeniusdemo.global
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+/**
+ * Created by Andraž Hribar on 9. 11. 2019.
+ * andraz.hribar@gmail.com
+ */
+@GlideModule
+class BeeniusGlideModule : AppGlideModule()
